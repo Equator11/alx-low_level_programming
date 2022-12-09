2a5c7d4da87b648ceba37face33d0bb7cc8d3763
Equator11 <vincentukam1@gmail.com>
2022-12-09 04:07:31 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0
@@ -7,6 +6,5 @@
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-		return (0);
-
+	return (0);
 }
