8a9156b0b484e95c8fda23d5b8faa3d91e905fd4
Equator11 <vincentukam1@gmail.com>
2022-12-12 14:19:36 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - prints exactly and that piece of art is useful
  *
+ * Description: The last project on hello world
+ *
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
 	return (1);
 }
