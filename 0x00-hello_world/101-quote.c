da27c576fd37fa93f9dcd2004e917e297bf93750
Equator11 <vincentukam1@gmail.com>
2022-12-12 16:07:36 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: The las task on hello world
+ * Description: The last task on hello world
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(0, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
