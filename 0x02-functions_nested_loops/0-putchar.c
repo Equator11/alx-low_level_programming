5d0123fa521dfc3379f20b8f11fe9a1ac4f5ecc6
Equator11 <vincentukam1@gmail.com>
2022-12-13 13:13:03 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Descriptiom: print _puthar followed by a new lone
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
